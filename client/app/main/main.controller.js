9fc0a48c8f9fc224a809d498e0f63b8ac0798009
Rory <rory88@gmail.com>
2015-11-02 19:57:43 +0000

    improved string builder, attempted x axis label

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,83 +2,104 @@
 
 angular.module('stockchartApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
+    //**************************************************************
+    // Build a URL string for the query
+    //**************************************************************
     var apiCall = {
       urlBase: {
-        name: 'https://www.quandl.com/api/v3/datasets',
-        delimiter: '/'
+        delimiter: '',
+        name: 'https://www.quandl.com/api/v3/datasets'
       },
       quandlCategory: {
-        name: 'WIKI',
-        delimiter: '/'
+        delimiter: '/',
+        name: 'WIKI'
       },
       stock: {
-        name: 'AAPL',
-        delimiter: '.'
+        delimiter: '/',
+        name: 'MSFT'
       },
       apiFileType: {
-        name: 'json',
-        delimiter: '?'
+        delimiter: '.',
+        name: 'json'
       },
       startDate: {
-        name: 'start_date=2014-10-01',
-        delimiter: '?'
+        delimiter: '?',
+        // TODO: Make the start date a date set to one month ago using Date object
+        name: 'start_date=2015-10-01'
       },
+      order: {
+        delimiter: '&',
+        name: 'order=asc'
+      },
+      //limit:{
+      //  delimiter: '?',
+      //  name: 'limit=20'
+      //},
       apiKey: {
-        name: 'api_key=zwfVKsRK7iy4KCdzcXaG',
-        delimiter: ''
+        delimiter: '&',
+        name: 'api_key=zwfVKsRK7iy4KCdzcXaG'
       }
     };
 
-    var apiCallUrl = '';
     var apiArr = [];
+    var apiCallUrl = '';
 
     Object.keys(apiCall).forEach(function (prop) {
       apiArr.push(apiCall[prop])
     });
 
     apiArr.forEach(function (element) {
-      apiCallUrl += element.name + element.delimiter;
+      apiCallUrl += element.delimiter + element.name;
     });
 
     console.log(apiCallUrl);
 
+    //**************************************************************
+    // Request data from Quandl
+    //**************************************************************
     $http.get(apiCallUrl)
       .success(function (data) {
 
         var myData = data.dataset;
 
-        //Make dates array
+        // Make dates array
         myData.dates = [];
         myData.data.forEach(function (item) {
           myData.dates.push(item[0]);
         });
 
-        //Make plot array
+        // Make plot array
         myData.plots = [];
         myData.data.forEach(function (item) {
           myData.plots.push(item[5]);
         });
 
+        // View url in console
         console.log(myData);
 
+        // Declare the c3 chart
         var chart = c3.generate({
+
           data: {
-            rows:[
-              ['AAPL'],
-              myData.plots
-            ]
+            json: {
+              data1: myData.plots
+            },
+            keys: {
+              x: myData.dates
+            }
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              tick: {
+                format: '%Y-%m-%d'
+              }
+            }
           }
         });
+      })
+      .error(function (err) {
+        console.log('There was a problem: ', err);
       });
 
 
