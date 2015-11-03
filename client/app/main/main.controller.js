03902e79036ef60cffb2e2905432d995c7f44671
Rory <rory88@gmail.com>
2015-11-03 14:58:33 +0000

    added x label and prepared possibility of further stocks

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -16,7 +16,7 @@ angular.module('stockchartApp')
       },
       stock: {
         delimiter: '/',
-        name: 'MSFT'
+        name: 'AAPL'
       },
       apiFileType: {
         delimiter: '.',
@@ -68,11 +68,17 @@ angular.module('stockchartApp')
           myData.dates.push(item[0]);
         });
 
+        myData.dates.unshift('x');
+        console.log('dates: ', myData.dates);
+
         // Make plot array
         myData.plots = [];
         myData.data.forEach(function (item) {
           myData.plots.push(item[5]);
         });
+
+        myData.plots.unshift(myData.name);
+        console.log('plots: ', myData.plots);
 
         // View url in console
         console.log(myData);
@@ -85,15 +91,14 @@ angular.module('stockchartApp')
 
         console.log(jsonArr);
 
-
         // Declare the c3 chart
         var chart = c3.generate({
           data: {
-            json: jsonArr,
-            keys: {
-              x: 'name',
-              value: ['val']
-            }
+            x: 'x',
+            columns: [
+              myData.dates,
+              myData.plots
+            ]
           },
           axis: {
             x: {
@@ -104,6 +109,41 @@ angular.module('stockchartApp')
             }
           }
         });
+
+        setTimeout(function () {
+          chart.load({
+            columns: [
+              ['data3', 400, 500, 450000, 7000000, 600, 50000000]
+            ]
+          });
+        }, 3000);
+
+        setTimeout(function () {
+          chart.unload({
+            ids: ['data3']
+          });
+        }, 3000);
+
+
+        // Declare the c3 chart
+        //var chart = c3.generate({
+        //  data: {
+        //    json: jsonArr,
+        //    keys: {
+        //      x: 'name',
+        //      value: ['val']
+        //    }
+        //  },
+        //  axis: {
+        //    x: {
+        //      type: 'timeseries',
+        //      tick: {
+        //        format: '%Y-%m-%d'
+        //      }
+        //    }
+        //  }
+        //});
+
       })
       .error(function (err) {
         console.log('There was a problem: ', err);
@@ -113,5 +153,6 @@ angular.module('stockchartApp')
   });
 
   // TODO: implement model to accept multiple stocks to chart
+  // TODO: Change number format from 8000000 to 8.0, or similar
   // TODO: label 'val' to be the stock in question
   // TODO: implement input and backend
